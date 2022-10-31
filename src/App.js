c416712fc51893cb4aa01a86ecc49e6464eb15c5
Sergen <dmrsrgn@gmail.com>
2022-10-31 12:04:55 -0500

    change layout styling to account for all screen sizes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="w-full mx-auto sm:w-3/4 md:w-1/2 min-h-screen bg-slate-700 p-4">
-      <Form />
+    <div className="w-full   min-h-screen bg-slate-700 p-4">
+      <div className=" mx-auto sm:w-3/4 md:w-2/3 lg:w-1/3 ">
+        <Form />
+      </div>
     </div>
   );
 }
